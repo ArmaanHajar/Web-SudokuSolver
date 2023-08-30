195f9bed2a3bc2314504bfb4fc2247b3fcbcfbc9
ArmaanHajar <armaanhajar@icloud.com>
2023-08-30 15:03:58 -0700

    board created in html

---
+++ views.py
@@ -0,0 +1,18 @@
+from flask import Blueprint, render_template
+
+views = Blueprint(__name__, "views")
+
+@views.route('/')
+def home():
+    board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 6, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 3],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 6],
+        [0, 0, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 5],
+        [0, 0, 0, 0, 0, 0, 0, 7, 9]
+    ]
+    return render_template("index.html", puzzle=board)
